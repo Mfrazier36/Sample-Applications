63869e478e75e3463a70b6e4984509d382a6478f
Myles <Myles.Frazier36@gmail.com>
2022-02-07 16:36:16 -0500

    Added empty list message

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -40,6 +40,11 @@ function App() {
     setEditText(props.text);
   }
 
+  const discardChanges = props => {
+    toggleEdit(props,false);
+    clearEditText();
+  }
+
   const saveChanges = props => {
     toggleEdit(props,false);
     var todoList = []
@@ -52,11 +57,6 @@ function App() {
       todoList.push(todo);
     }
     setTodoItems(todoList);
-    clearEditText();
-  }
-
-  const discardChanges = props => {
-    toggleEdit(props,false);
     clearEditText();
   }
 
@@ -78,18 +78,18 @@ function App() {
           <section className="container mt-5">
             <div className="row justify-content-center">
               <div className="col-4 mx-auto">
-                <ul className="container py-5 bg-dark text-white rounded">
-                  <h1 className="border-bottom border-white mb-4">
+                <ul className="container bg-dark text-white rounded">
+                  <h1 className="row border-bottom border-white py-4 mb-3">
                     <strong>
                       Todo List
                     </strong>
                   </h1>
 
-                  {(todoItems.map((item) => (
-                    <li className="row my-2 py-1 bg-secondary rounded" key={item.id}>
+                  {todoItems.length > 0 
+                  ? todoItems.map((item) => (
+                    <li className="row bg-secondary rounded my-3 py-1" key={item.id}>
                       <section className="row py-2">
                         <div className="col" id={"todoDisplay" + String(item.id)} style={{letterSpacing:"1px", lineHeight:"2em"}}>
-                          <p>
                             {item.text.split('\n').map(x => (
                               <>
                                 <div>
@@ -97,43 +97,54 @@ function App() {
                                 </div>
                               </>
                             ))}
-                          </p>
                         </div>
                         <div className="col pt-1" id={"todoEditInput" + String(item.id)} style={{letterSpacing:"1px"}} hidden>
-                          <textarea className="w-100" value={editTextInput} onChange={(e) => setEditText(e.target.value)} rows={3}></textarea>
+                          <textarea className="w-100" 
+                              value={editTextInput} 
+                              onChange={(e) => setEditText(e.target.value)} 
+                              rows={3}
+                          ></textarea>
                         </div>
                       </section>
                       <section className="row py-2" id={"editToolbar" + String(item.id)} hidden>
                         <div className="col-2">
-                          <button title="Save Changes" className="btn btn-success w-100 rounded" type="button" onClick={() => saveChanges(item)}>
+                          <button title="Save Changes" className="btn btn-success rounded w-100" type="button" onClick={() => saveChanges(item)}>
                             &#10004;
                           </button>
                         </div>
                         <div className="col-2">
-                          <button title="Discard Changes" className="btn btn-danger w-100 rounded" type="button" onClick={() => discardChanges(item)}>
+                          <button title="Discard Changes" className="btn btn-danger rounded w-100" type="button" onClick={() => discardChanges(item)}>
                             &#10006;
                           </button>
                         </div>
                       </section>
                       <section className="row py-2" id={"todoToolbar" + String(item.id)}>
                         <div className="col-2">
-                          <button title="Edit Todo Item" className="btn btn-primary w-100 rounded" type="button" onClick={() => editTodoItem(item)}>
+                          <button title="Edit" className="btn btn-primary rounded w-100" type="button" onClick={() => editTodoItem(item)}>
                             &#9998;
                           </button>
                         </div>
                         <div className="col-2">
-                          <button title="Delete Todo Item" className="btn btn-danger w-100 rounded" type="button" onClick={() => removeTodoItem(item)}>
+                          <button title="Delete" className="btn btn-danger rounded w-100" type="button" onClick={() => removeTodoItem(item)}>
                             &#10006;
                           </button>
                         </div>
                       </section>
                     </li>
-                  )))}
+                  ))
+                : (
+                  <div className="row text-center bg-secondary rounded my-3 py-2">
+                    <div className="col">
+                      <h5>
+                        You dont have any items
+                      </h5>
+                    </div>
+                  </div>
+                )}
 
-                  <li className="row mt-5" id="InputRow">
+                  <section className="row border-top border-white mt-3 py-3" id="InputRow">
                     <div className="col">
                       <textarea className="w-100" 
-                          type="text" 
                           value={textInput} 
                           onChange={(e) => setText(e.target.value)} 
                           rows={4}
@@ -147,7 +158,7 @@ function App() {
                         Clear
                       </button>
                     </div>
-                  </li>
+                  </section>
                 </ul>
               </div>
             </div>
